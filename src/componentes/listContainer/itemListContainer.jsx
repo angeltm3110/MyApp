0a910866101aa693222c684d9697ed67c5393a27
angel tirado <angeltm3195@gmail.com>
2021-09-27 15:24:40 -0400

    Agregue el menu

---
+++ src/componentes/listContainer/itemListContainer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function itemListContainer(props) {
+    return(
+
+        <>
+            <h2>{props.greeting} {props.usuario}</h2>
+        </>
+    );
+
+}
+
---
+++ src/componentes/navbar/navbar.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+
+
+export default function Menu() {
+    return(
+        <nav className="navbar navbar-expand-lg navbar-light bg-light fs-4">
+            <div className="container-fluid">
+             <a className="navbar-brand" href="#">Naggel</a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+        
+                <li className="nav-item">
+                <a className="nav-link" href="#">Nuestros Productos</a>
+                </li>
+                <li className="nav-item">
+                <a className="nav-link" href="#">Pedidos</a>
+                </li>
+                <li className="nav-item">
+                <a className="nav-link disabled">Contacto</a>
+                </li>
+            </ul>
+            </div>
+        </div>
+        </nav>
+    );
+
+}
